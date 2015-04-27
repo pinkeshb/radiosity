841d687eb16a10ffdee56c5ef9181bf45c8c4de4
Pinkesh Barsopia <barsopia@gmail.com>
2015-04-27 15:31:53 +0530

    m2 wavelet+removed some bugs from dwt added idwt

--- 2D/m2/wavelet/main.py
+++ 2D/m2/wavelet/main.py
@@ -3,6 +3,7 @@ from mpmath import *
 import time
 import datetime
 import sys
+from wavedecs import *
 
 
 n=4
@@ -10,40 +11,63 @@ n=4
 
 ts=time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-fname=str(n)+'haar_scale'+".txt"
+fname=str(n)+'haar_sscale'+".txt"
 fo = open(fname, "w")
 
 
 K=project_kernel_m2_phi(n)
+K_dwt=dwt2(K)
+
 E=matrix([[1.0/pow(n,0.5)]*n*2]).transpose()
 for i in range(n*2):
 	if i%2==1:
 		E[i]=0
+E_dwt=dwt(E)
 B=zeros(2*n,1)
+B_dwt=zeros(2*n,1)
 G=zeros(2*n,1)
 
 
 print "K="
 print  K
+print "K_dwt="
+print  K_dwt
+for i in range(n*2):
+	for j in range(n*2):
+		if abs(K_dwt[i,j])<0.05:
+			K_dwt[i,j]=0
+print "K_dwt="
+print  K_dwt
 print "E=", E
+print len(E)
+print len(E_dwt)
+print "E_dwt=", E_dwt,"fin"
 fo.write("n="+str(n)+"\n")
 fo.write("K="+str(K)+"\n")
 fo.write("E="+str(E)+"\n")
-B_pre=B+1
-
+B_pre=B+0.1
+B_pre
 
 iter = 1
-while(not diag(B)*B-diag(B_pre)*B_pre == zeros(2*n,1)):
-	print diag(B)*B-diag(B_pre)*B_pre
-	print "B"+str(iter)+"=", B
+while(not diag(B_dwt)*B_dwt-diag(B_pre)*B_pre == zeros(2*n,1)):
+	print "B_dwt"+str(iter)+"=", B_dwt
 	fo.write("B"+str(iter)+"="+str(B)+"\n")
-	B_pre=B
-	B=E+K*B_pre
+	B_pre=B_dwt
+	B_dwt=E_dwt+K_dwt*B_pre
 
 	# if int(input("Please enter an integer: "))==1:
 	# 	check=True
 	# else:
 	# 	check=False
+	print "B_pre"+str(iter)+"=", B_pre
+	print "B_dwt"+str(iter)+"=", B_dwt
+	print "error"
+	print diag(B_dwt)*B_dwt-diag(B_pre)*B_pre
+	print "error"
 	iter+=1
-print "\n\n\nB"+str(iter)+"=", B*pow(n,0.5)
-# fo.write("\n\n\nB"+str(iter)+"="+str(B*pow(n,0.5)))
+print "final solution"
+print "\n\n\nB"+str(iter)+"=", B_dwt
+fo.write("\n\n\nB"+str(iter)+"="+str(B_dwt))
+print "\n\n\nB"+str(iter)+"=", idwt(B_dwt)
+fo.write("\n\n\nB"+str(iter)+"="+str(idwt(B_dwt)))
+
--- 2D/m2/wavelet/wavedecs.py
+++ 2D/m2/wavelet/wavedecs.py
@@ -1,21 +1,33 @@
 import pywt
 from mpmath import *
 import copy
-root3=1.732050808
-def dwt(E):
+import math
+root3=pow(3,0.5)
+root2=pow(2,0.5)
+root8=root2*2
+def dwt_inside(E):
 	# print E
 	length=len(E)
 	E_dwt=[0]*length
 	for i in range(length/4):
-		print i,"i"
-		E_dwt[2*i]=2*E[4*i]+2*E[4*i+2]
-		E_dwt[2*i+1]=-root3*E[4*i]+E[4*i+1]+root3*E[4*i+2]+E[4*i+3]
+		# print i,"i"
+		E_dwt[2*i]=(2*E[4*i]+2*E[4*i+2])/root8
+		E_dwt[2*i+1]=(-root3*E[4*i]+E[4*i+1]+root3*E[4*i+2]+E[4*i+3])/root8
 	for i in range(length/4):
-		E_dwt[2*i+length/4]=-2*E[4*i]+2*E[4*i+2]
-		E_dwt[2*i+1]=E[4*i]+root3*E[4*i+1]-E[4*i+2]+root3*E[4*i+3]
+		E_dwt[2*i+length/2]=(-2*E[4*i+1]+2*E[4*i+3])/root8
+		E_dwt[2*i+1+length/2]=(E[4*i]+root3*E[4*i+1]-E[4*i+2]+root3*E[4*i+3])/root8
+
 
 	# print E_dwt
 	return matrix(E_dwt)
+def dwt(E):
+	length=len(E)
+	E_dwt=copy.deepcopy(E)
+	E_dwt=dwt_inside(E)
+	if length>4:
+		for i in range(2,int(math.log(length,2))):
+			E_dwt[0:length-pow(2,i)]=dwt_inside(E_dwt[0:length-pow(2,i)])
+	return E_dwt
 
 def dwt2(K):
 	K_dwt=copy.deepcopy(K)
@@ -26,19 +38,44 @@ def dwt2(K):
 		K_dwt[i,:]=dwt(K_dwt_i[i,:]).transpose()
 	return K_dwt
 
-def idwt(E):
+def idwt_inside(E):
 	# print E
-	E_rep=E.tolist()[0:2]
-	for i in range(1,int(pow(len(E),0.5))):
-		E_rep.append(E[pow(2,i):pow(2,i+1)])
-	E_idwt=pywt.waverec(E_rep,'db1')
+	# print type(E)
+	# print type(E)
+
+	length=len(E)
+	E_idwt=[0]*length
+	# print type(E[2*0])
+	for i in range(length/4):
+		# print i ,"i"
+		# print E[2*i],E[2*i+1],E[length/2+2*i],E[length/2+2*i+1]
+		E_idwt[4*i]=E[2*i]/root2-E[2*i+1]*root3/root8+E[length/2+2*i]*0+E[length/2+2*i+1]/root8
+		E_idwt[4*i+1]=E[2*i]*0+E[2*i+1]/root8-E[length/2+2*i]/root2+E[length/2+2*i+1]*root3/root8
+		E_idwt[4*i+2]=E[2*i]/root2+E[2*i+1]*root3/root8+E[length/2+2*i]*0-E[length/2+2*i+1]/root8
+		E_idwt[4*i+3]=E[2*i]*0+E[2*i+1]/root8+E[length/2+2*i]/root2+E[length/2+2*i+1]*root3/root8
+
 	# print E_idwt
 	return matrix(E_idwt)
+def idwt(E):
+	length=len(E)
+	E_idwt=copy.deepcopy(E)
+	# E_dwt=dwt_inside(E)
+	# if length>4:
+	# 	for i in range(2,int(math.log(length,2))):
+	# 		E_dwt[0:length-pow(2,i)]=dwt_inside(E_dwt[0:length-pow(2,i)])
+	for i in range(2,int(math.log(length,2))+1):
+		E_idwt[0:pow(2,i)]=idwt_inside(E_idwt[0:pow(2,i)])
+
+
+	return E_idwt
 if __name__=="__main__":
-	K=matrix([
-[1,0,1,0],[0,0,0,0],[1,0,1,0],[0,0,0,0]
-		])
-	# print E
-	print dwt2(K)
+# 	K=matrix([
+# [1,0,1,0],[0,0,0,0],[1,0,1,0],[0,0,0,0]
+# 		])
+	E=matrix([[1,1,1,1,1,1,1,0]])
+	# E=matrix([ 0.288461538460848,0.0333086693845357, 0.403846153845187,0.0333086693845357, 0.519230769229526,0.0333086693845357, 0.634615384613866, 0.0333086693845357])
+	print E,"\n"
+	print dwt_inside(E),"\n"
+	print idwt_inside(dwt_inside(E)),"\n"
 	# print E[0]
 
