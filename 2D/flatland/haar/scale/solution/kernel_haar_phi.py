4eb603a0b1a4532bc996abcf14a654ff07efbdb2
Pinkesh Barsopia <barsopia@gmail.com>
2015-05-05 16:24:17 +0530

    dist in kernel flatland haar scale sol lernel_haar_phi

--- 2D/flatland/haar/scale/solution/kernel_haar_phi.py
+++ 2D/flatland/haar/scale/solution/kernel_haar_phi.py
@@ -3,7 +3,8 @@ from mpmath import *
 
 def project_kernel_haar_phi(n):
     # kernel
-    f1 = lambda s, t: 1/ (2*pow(((s - t) * (s - t) + 1), 1.5))
+    dist=1
+    f1 = lambda s, t: dist*dist/ (2*pow(((s - t) * (s - t) + dist*dist), 1.5))
     f = lambda s, t: f1(s, t) * pow(n, 0.5) * pow(n, 0.5)
 
     K = matrix(n, n)
@@ -15,3 +16,5 @@ def project_kernel_haar_phi(n):
                 i / float(n), (i + 1) / float(n)], [j / float(n), (j + 1) / float(n)]
 
     return K
+
+print project_kernel_haar_phi(8)
